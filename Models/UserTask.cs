690d5ee92cf8927c8883823778f301b67f1b7f98
Zach Filla <fillazj@mail.uc.edu>
2025-07-13 23:00:48 -0400

    basic Todo List page with CV list
    - added UserTask data model
    - set up TodoPage with CollectionView (still need to add bindings)

---
+++ Models/UserTask.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TaskFlow.Models
+{
+    public class UserTask
+    {
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public DateTime DueDateTime { get; set; }
+        public bool IsCompleted { get; set; }
+
+        public UserTask(string name = "New Task", string description = "default description", DateTime dueDateTime = new DateTime(), bool isCompleted  = false) {
+            Name = name;
+            Description = description;
+            IsCompleted = isCompleted;
+            
+            // set due date to Now if no dueDateTime is given
+            if (dueDateTime == new DateTime())
+            {
+                DueDateTime = DateTime.Now;
+            }
+            else
+            {
+                DueDateTime = dueDateTime;
+            }
+        }
+    }
+}
--- TodoPage.xaml.cs
+++ TodoPage.xaml.cs
@@ -1,9 +1,27 @@
+using TaskFlow.Models;
+
 namespace TaskFlow;
 
 public partial class TodoPage : ContentPage
 {
+	public List<UserTask> UserTasks = new List<UserTask>();
 	public TodoPage()
 	{
 		InitializeComponent();
-	}
+
+		const int demoListLength = 5;
+
+        for (int i = 0; i < demoListLength; i++)
+        {
+            UserTasks.Add(new UserTask());
+        }
+
+        Cv_TodoList.ItemsSource = UserTasks;
+
+    }
+
+    private void Cv_TodoList_SelectionChanged(object sender, SelectionChangedEventArgs e)
+    {
+        // TODO: mark task as complete
+    }
 }
